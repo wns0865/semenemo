56bf1c824b1fbd2c96cb3d9f4f0a3a4565dc4106
jaehan4707 <jahan4707@naver.com>
2024-09-09 23:18:17 +0900

    chore(#S11P21D109-134): app 모듈 build gradle 필요없는 의존성 제거

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     id("kotlin-kapt")
     alias(libs.plugins.hilt)
-    alias(libs.plugins.kotlinx.serialization)
 }
 
 android {
@@ -53,34 +52,10 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
     // hilt
     implementation(libs.hilt.android)
-    implementation(libs.androidx.hilt.navigation.compose)
     kapt(libs.hilt.compiler)
-    // retrofit, okhttp, gson
-    implementation(libs.bundles.network)
-    // coroutines
-    implementation(libs.bundles.coroutines)
-    // preference datastore
-    implementation(libs.datastore.preferences)
-    // lottie
-    implementation(libs.lottie)
-
+    implementation(libs.androidx.hilt.navigation.compose)
     implementation(project(":domain"))
     implementation(project(":data"))
     implementation(project(":presentation"))
