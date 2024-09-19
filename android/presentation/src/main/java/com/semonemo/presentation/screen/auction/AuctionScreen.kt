a68558c25e518abee064fe562220f465edb68e5b
전형선 <hsun5163@gmail.com>
2024-09-19 15:31:00 +0900

    rename(#S11P21D109-115): TextLabel -> SectionHeader 변경

--- android/presentation/src/main/java/com/semonemo/presentation/screen/auction/AuctionScreen.kt
+++ android/presentation/src/main/java/com/semonemo/presentation/screen/auction/AuctionScreen.kt
@@ -8,7 +8,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.semonemo.presentation.R
-import com.semonemo.presentation.component.TextLabel
+import com.semonemo.presentation.component.SectionHeader
 import com.semonemo.presentation.screen.auction.subScreen.LongAuctionReadScreen
 import com.semonemo.presentation.screen.auction.subScreen.ShortAuctionReadScreen
 
@@ -16,10 +16,10 @@ import com.semonemo.presentation.screen.auction.subScreen.ShortAuctionReadScreen
 fun AuctionScreen() {
     Column {
         Spacer(modifier = Modifier.height(30.dp))
-        TextLabel(text = stringResource(R.string.in_progress_short_action))
+        SectionHeader(text = stringResource(R.string.in_progress_short_action))
         ShortAuctionReadScreen()
         Spacer(modifier = Modifier.height(30.dp))
-        TextLabel(text = stringResource(R.string.in_progress_long_action))
+        SectionHeader(text = stringResource(R.string.in_progress_long_action))
         LongAuctionReadScreen()
     }
 }
