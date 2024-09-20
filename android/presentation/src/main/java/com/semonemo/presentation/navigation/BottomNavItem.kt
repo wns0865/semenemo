5e6e4931539424ce7aaf968c26279b3df6400929
NAKYUNG LEE <lnk12888@naver.com>
2024-09-20 14:38:43 +0900

    ui(#S11P21D109-168): BottomNavItem 클래스 구현

---
+++ android/presentation/src/main/java/com/semonemo/presentation/navigation/BottomNavItem.kt
@@ -0,0 +1,40 @@
+package com.semonemo.presentation.navigation
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.semonemo.presentation.R
+
+sealed class BottomNavItem(
+    @StringRes val title: Int,
+    @DrawableRes val icon: Int,
+    @DrawableRes val iconSelected: Int,
+    val route: String,
+) {
+    data object Shop : BottomNavItem(
+        title = R.string.shop_title,
+        icon = R.drawable.ic_bot_nav_outline_shop,
+        iconSelected = R.drawable.ic_bot_nav_fill_shop,
+        route = ScreenDestinations.Shop.route,
+    )
+
+    data object Auction : BottomNavItem(
+        title = R.string.auction_title,
+        icon = R.drawable.ic_bot_nav_outline_auction,
+        iconSelected = R.drawable.ic_bot_nav_fill_auction,
+        route = ScreenDestinations.Auction.route,
+    )
+
+    data object Coin : BottomNavItem(
+        title = R.string.coin_title,
+        icon = R.drawable.ic_bot_nav_outline_coin,
+        iconSelected = R.drawable.ic_bot_nav_fill_coin,
+        route = ScreenDestinations.Coin.route,
+    )
+
+    data object MyPage : BottomNavItem(
+        title = R.string.my_title,
+        icon = R.drawable.ic_bot_nav_outline_profile,
+        iconSelected = R.drawable.ic_bot_nav_fill_profile,
+        route = ScreenDestinations.MyPage.route,
+    )
+}
