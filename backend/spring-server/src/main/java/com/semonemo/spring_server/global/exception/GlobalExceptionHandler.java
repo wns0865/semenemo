ab37acf0510da39ad8bcc7cc08e8e76b49a44421
이지언 <dlwldjs7544@naver.com>
2024-09-20 10:45:02 +0900

    refactor(#S11P21D109-163): 공통 응답/에러 객체 리팩토링

--- backend/spring-server/src/main/java/com/semonemo/spring_server/global/exception/GlobalExceptionHandler.java
+++ backend/spring-server/src/main/java/com/semonemo/spring_server/global/exception/GlobalExceptionHandler.java
@@ -4,20 +4,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.semonemo.spring_server.global.CommonResponse;
-
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(CustomException.class)
-	public ResponseEntity<CommonResponse<?>> handleCustomException(CustomException e) {
+	public ResponseEntity<ErrorResponse<?>> handleCustomException(CustomException e) {
 		ErrorCode errorCode = e.getErrorCode();
+
 		return new ResponseEntity<>(
-			CommonResponse.builder()
-				.status(errorCode.getCode())
-				.data(null)
-				.message(errorCode.getMessage())
-				.build(),
+			ErrorResponse.error(errorCode),
 			errorCode.getHttpStatus()
 		);
 	}
