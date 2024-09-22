227a08e958840be1197e71365ccf9f3c4382df45
nakyung128 <lnk12888@naver.com>
2024-09-22 23:39:20 +0900

    ui(#S11P21D109-191): statusBar, navigationBar padding 추가

--- android/presentation/src/main/java/com/semonemo/presentation/screen/mypage/DetailScreen.kt
+++ android/presentation/src/main/java/com/semonemo/presentation/screen/mypage/DetailScreen.kt
@@ -10,8 +10,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyRow
@@ -102,11 +104,16 @@ fun DetailScreen(
                 .fillMaxSize()
                 .background(brush = Main01)
                 .verticalScroll(state = scrollState)
+                .statusBarsPadding()
+                .navigationBarsPadding()
                 .padding(horizontal = 25.dp, vertical = 18.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Box(
-            modifier = Modifier.fillMaxWidth(),
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp),
             contentAlignment = Alignment.Center,
         ) {
             Text(
