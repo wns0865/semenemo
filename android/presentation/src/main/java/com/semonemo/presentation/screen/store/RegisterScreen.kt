11ae3f8f17a66c6947dd544a25f6f8ff795de185
NAKYUNG LEE <lnk12888@naver.com>
2024-09-23 17:54:37 +0900

    ui(#S11P21D109-192): 프레임 등록 페이지 UI 제작

---
+++ android/presentation/src/main/java/com/semonemo/presentation/screen/store/RegisterScreen.kt
@@ -0,0 +1,329 @@
+package com.semonemo.presentation.screen.store
+
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.semonemo.presentation.R
+import com.semonemo.presentation.component.CustomTab
+import com.semonemo.presentation.component.HashTag
+import com.semonemo.presentation.component.HashTagTextField
+import com.semonemo.presentation.component.LongUnableButton
+import com.semonemo.presentation.component.PriceTextField
+import com.semonemo.presentation.theme.Gray01
+import com.semonemo.presentation.theme.Gray02
+import com.semonemo.presentation.theme.Gray03
+import com.semonemo.presentation.theme.SemonemoTheme
+import com.semonemo.presentation.theme.Typography
+import com.semonemo.presentation.util.addFocusCleaner
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RegisterScreen() {
+    Surface(
+        modifier =
+            Modifier
+                .fillMaxSize()
+                .background(color = Color.White),
+    ) {
+        val focusManager = LocalFocusManager.current
+        val scrollState = rememberScrollState()
+        var price by remember { mutableStateOf("") }
+        var showBottomSheet by remember { mutableStateOf(false) }
+        var selectedIndex by remember { mutableIntStateOf(0) }
+
+        // 더미 데이터
+        val tags =
+            remember {
+                mutableStateListOf(
+                    "플레이브",
+                    "도은호",
+                )
+            }
+        val tabs = listOf("1x1", "1x4", "2x2")
+
+        val frames1 =
+            listOf(
+                R.drawable.img_example,
+                R.drawable.img_example2,
+                R.drawable.img_example3,
+                R.drawable.img_example,
+                R.drawable.img_example2,
+                R.drawable.img_example3,
+            )
+
+        val frames2 =
+            listOf(
+                R.drawable.img_example3,
+                R.drawable.img_example2,
+                R.drawable.img_example,
+                R.drawable.img_example3,
+                R.drawable.img_example2,
+                R.drawable.img_example,
+                R.drawable.img_example3,
+                R.drawable.img_example2,
+                R.drawable.img_example,
+            )
+
+        val frames3 =
+            listOf(
+                R.drawable.img_example,
+                R.drawable.img_example2,
+                R.drawable.img_example3,
+            )
+
+        val frames = listOf(frames1, frames2, frames3)
+
+        Column(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 20.dp, vertical = 15.dp)
+                    .statusBarsPadding()
+                    .navigationBarsPadding()
+                    .verticalScroll(state = scrollState)
+                    .addFocusCleaner(
+                        focusManager = focusManager,
+                    ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = "프레임 판매",
+                style = Typography.bodyMedium.copy(fontSize = 20.sp),
+                textAlign = TextAlign.Center,
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Surface(
+                modifier =
+                    Modifier
+                        .width(200.dp)
+                        .height(300.dp),
+                border = BorderStroke(width = 2.dp, color = Gray01),
+                shape = RoundedCornerShape(10.dp),
+                onClick = {
+                    showBottomSheet = true
+                },
+            ) {
+                Column(
+                    modifier =
+                        Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_frame_plus),
+                        contentDescription = "ic_frame_plus",
+                        tint = Color.Unspecified,
+                    )
+                    Spacer(modifier = Modifier.height(9.dp))
+                    Text(
+                        text = "프레임을 추가해 주세요",
+                        style = Typography.bodySmall.copy(fontSize = 15.sp),
+                        color = Gray02,
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(30.dp))
+            // 프레임 추가한 경우 상품명/설명 필드 이 위치에 추가됨
+            Box(
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight(),
+                contentAlignment = Alignment.CenterStart,
+            ) {
+                Text(
+                    text = "태그",
+                    style = Typography.titleMedium.copy(fontSize = 16.sp),
+                )
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            HashTagTextField(
+                onTagAddAction = { keyword ->
+                    if (keyword.isNotEmpty()) {
+                        tags.add(keyword)
+                    }
+                },
+                focusManager = focusManager,
+            )
+            LazyRow(
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                        .padding(top = 10.dp),
+                horizontalArrangement = Arrangement.spacedBy(5.dp),
+                content = {
+                    items(tags.size) { index ->
+                        HashTag(
+                            keyword = tags[index],
+                            isEdit = true,
+                            onCloseClicked = {
+                                tags.remove(tags[index])
+                            },
+                        )
+                    }
+                },
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Box(
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight(),
+                contentAlignment = Alignment.CenterStart,
+            ) {
+                Text(
+                    text = "구매가",
+                    style = Typography.titleMedium.copy(fontSize = 16.sp),
+                )
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            PriceTextField(
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .height(48.dp),
+                price = price,
+                onPriceChange = { newPrice ->
+                    price = newPrice
+                },
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            // 프레임 불러오기 Success면 LongBlackButton
+            // 다른 상태면 LongUnableButton
+            LongUnableButton(
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 20.dp),
+                text = "판매 등록",
+            )
+        }
+        if (showBottomSheet) {
+            ModalBottomSheet(
+                onDismissRequest = { showBottomSheet = false },
+                containerColor = Color.White,
+                shape = RoundedCornerShape(10.dp),
+            ) {
+                Column(
+                    modifier =
+                        Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight(0.45f),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Spacer(modifier = Modifier.height(10.dp))
+                    CustomTab(
+                        tabList = tabs,
+                        selectedIndex = selectedIndex,
+                        onTabSelected = { tab -> selectedIndex = tab },
+                    )
+                    AnimatedContent(
+                        targetState = selectedIndex,
+                        transitionSpec = {
+                            (slideInHorizontally { width -> width } + fadeIn())
+                                .togetherWith(
+                                    slideOutHorizontally { width -> -width } + fadeOut(),
+                                ).using(
+                                    SizeTransform(clip = false),
+                                )
+                        },
+                        label = "",
+                    ) { targetIndex ->
+                        LazyVerticalGrid(
+                            modifier =
+                                Modifier
+                                    .fillMaxWidth()
+                                    .wrapContentHeight()
+                                    .padding(horizontal = 20.dp, vertical = 20.dp),
+                            columns = GridCells.Fixed(4),
+                            state = rememberLazyGridState(),
+                        ) {
+                            items(frames[targetIndex].size) { index ->
+                                Image(
+                                    painter = painterResource(id = frames[targetIndex][index]),
+                                    contentDescription = null,
+                                    contentScale = ContentScale.Crop,
+                                    modifier =
+                                        Modifier
+                                            .fillMaxWidth()
+                                            .aspectRatio(1f)
+                                            .padding(8.dp)
+                                            .clip(shape = RoundedCornerShape(10.dp))
+                                            .border(
+                                                width = 1.dp,
+                                                shape = RoundedCornerShape(10.dp),
+                                                color = Gray03,
+                                            ),
+                                )
+                            }
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(20.dp))
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun RegisterScreenPreview() {
+    SemonemoTheme {
+        RegisterScreen()
+    }
+}
