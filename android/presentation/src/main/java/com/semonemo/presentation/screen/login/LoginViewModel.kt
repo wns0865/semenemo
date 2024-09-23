bde52aa6eb8f5670b89378d74ceeb4ff5d0223db
jaehan4707 <jahan4707@naver.com>
2024-09-23 17:10:31 +0900

    feat(#S11P21D109-244): LoginViewModel 추가

--- android/presentation/src/main/java/com/semonemo/presentation/screen/login/LoginViewModel.kt
+++ android/presentation/src/main/java/com/semonemo/presentation/screen/login/LoginViewModel.kt
@@ -1,10 +1,14 @@
 package com.semonemo.presentation.screen.login
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.semonemo.domain.model.ApiResponse
 import com.semonemo.domain.repository.AuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -15,11 +19,42 @@ class LoginViewModel
     constructor(
         private val authRepository: AuthRepository,
     ) : ViewModel() {
+        private val _uiState = MutableStateFlow<LoginUiState>(LoginUiState.Init)
+        val uiState = _uiState.asStateFlow()
+        private val _uiEvent = MutableSharedFlow<LoginUiEvent>()
+        val uiEvent = _uiEvent.asSharedFlow()
+
+    /*
+     * TODO
+     * 유저 정보 로딩(지갑주소,비밀번호)
+     */
+        private fun loadUserInfo() {
+        }
+
         fun existUser(address: String) {
             viewModelScope.launch {
                 authRepository.exists(address).collectLatest { response ->
-                    // TODO 추후 Ui 반영 처리
-                    Log.d("jaehan", "response : $response")
+                    when (response) {
+                        is ApiResponse.Success -> {
+                            if (response.data) {
+                                _uiState.value =
+                                    LoginUiState.Loading(
+                                        isWalletLoading = true,
+                                        walletAddress = address,
+                                    )
+                            } else {
+                                _uiEvent.emit(LoginUiEvent.RequiredRegister(address))
+                            }
+                        }
+
+                        is ApiResponse.Error -> {
+                            _uiState.value =
+                                LoginUiState.Error(
+                                    errorCode = response.errorCode,
+                                    errorMessage = response.errorMessage,
+                                )
+                        }
+                    }
                 }
             }
         }
