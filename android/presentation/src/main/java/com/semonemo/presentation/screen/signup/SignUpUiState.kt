3d7b329baf18d84cc81a3dfbb71f63244b5dffd7
jaehan4707 <jahan4707@naver.com>
2024-09-23 23:17:34 +0900

    feat(#S11P21D109-245): SignUpUiState 추가

---
+++ android/presentation/src/main/java/com/semonemo/presentation/screen/signup/SignUpUiState.kt
@@ -0,0 +1,7 @@
+package com.semonemo.presentation.screen.signup
+
+data class SignUpUiState(
+    val nickname: String = "",
+    val password: String = "",
+    val profileImageUrl: String = "",
+)
