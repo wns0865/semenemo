8e0a0dad802a2dddb492b1aff17b3c1f1385888c
jaehan4707 <jahan4707@naver.com>
2024-09-23 23:55:44 +0900

    feat(#S11P21D109-245): validate 함수 추가

--- android/presentation/src/main/java/com/semonemo/presentation/screen/signup/SignUpUiState.kt
+++ android/presentation/src/main/java/com/semonemo/presentation/screen/signup/SignUpUiState.kt
@@ -1,7 +1,19 @@
 package com.semonemo.presentation.screen.signup
 
+import com.semonemo.presentation.util.Validator
+
 data class SignUpUiState(
     val nickname: String = "",
     val password: String = "",
     val profileImageUrl: String = "",
-)
+) {
+    fun validate(): Boolean =
+        profileImageUrl.isNotBlank() &&
+            nickname.isNotBlank() &&
+            password.isNotBlank() &&
+            Validator.validationNickname(nickname).isEmpty() &&
+            Validator
+                .validationPassword(
+                    password,
+                ).isEmpty()
+}
