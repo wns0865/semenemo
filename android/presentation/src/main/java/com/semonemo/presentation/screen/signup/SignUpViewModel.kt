0443c298ddc672a34e6503c0227272fc24814436
jaehan4707 <jahan4707@naver.com>
2024-09-24 15:08:02 +0900

    feat(#S11P21D109-246): SignUpViewModel validate 추가

--- android/presentation/src/main/java/com/semonemo/presentation/screen/signup/SignUpViewModel.kt
+++ android/presentation/src/main/java/com/semonemo/presentation/screen/signup/SignUpViewModel.kt
@@ -3,11 +3,21 @@ package com.semonemo.presentation.screen.signup
 import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.semonemo.domain.model.ApiResponse
 import com.semonemo.domain.repository.AuthRepository
+import com.semonemo.domain.request.SignUpRequest
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import java.io.File
 import javax.inject.Inject
 
 @HiltViewModel
@@ -20,10 +30,8 @@ class SignUpViewModel
         private val walletAddress = savedStateHandle.get<String>("walletAddress") ?: ""
         private val _uiState = MutableStateFlow(SignUpUiState())
         val uiState = _uiState.asStateFlow()
-
-        init {
-            Log.d("jaehan", "walletAddress $walletAddress")
-        }
+        private val _uiEvent = MutableSharedFlow<SignUpUiEvent>()
+        val uiEvent = _uiEvent.asSharedFlow()
 
         fun updateNickname(nickname: String) {
             _uiState.update {
@@ -41,11 +49,28 @@ class SignUpViewModel
             }
         }
 
-        fun updateProfileImageUrl(profileImageUrl: String) {
+        fun updateProfileImage(profileImage: File) {
             _uiState.update {
                 it.copy(
-                    profileImageUrl = profileImageUrl,
+                    profileImage = profileImage,
                 )
             }
         }
+
+        private suspend fun validate(nickname: String): Flow<Boolean> =
+            authRepository.validateNickname(nickname).map { response ->
+                when (response) {
+                    is ApiResponse.Success -> true
+
+                    is ApiResponse.Error -> {
+                        _uiEvent.emit(
+                            SignUpUiEvent.Error(
+                                code = response.errorCode,
+                                message = response.errorMessage,
+                            ),
+                        )
+                        false
+                    }
+                }
+            }
     }
