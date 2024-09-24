1fa8136ca26856b31f9fc359f1a819a7682d003e
jaehan4707 <jahan4707@naver.com>
2024-09-24 09:12:43 +0900

    feat(#S11P21D109-245): 비밀번호 정규식, 에러 메시지 변경

--- android/presentation/src/main/java/com/semonemo/presentation/util/ErrorMessage.kt
+++ android/presentation/src/main/java/com/semonemo/presentation/util/ErrorMessage.kt
@@ -1,9 +1,9 @@
 package com.semonemo.presentation.util
 
-//^[가-힣a-zA-Z0-9_]{1,15}$
 object ErrorMessage {
-    const val EMPTY_MESSAGE="공백을 제외해주세요"
-    const val TOO_SHORT = "최소 2글자를 입력해주세요"
+    const val EMPTY_MESSAGE = "공백을 제외해주세요"
+    const val NICK_TOO_SHORT = "최소 2글자를 입력해주세요"
+    const val PW_TOO_SHORT = "최소 2글자를 입력해주세요"
     const val NICK_TOO_LONG = "최대 10글자만 가능해요"
     const val PW_TOO_LONG = "최대 15글자만 가능해요"
     const val NICK_NOT_MATCH = "한글,영어,숫자,특수기호(-,_)만 가능해요"
--- android/presentation/src/main/java/com/semonemo/presentation/util/Validator.kt
+++ android/presentation/src/main/java/com/semonemo/presentation/util/Validator.kt
@@ -2,13 +2,13 @@ package com.semonemo.presentation.util
 
 object Validator {
     private val nickNameRegex = "^[가-힣a-zA-Z0-9_]{1,15}$".toRegex()
-    private val passwordRegex = "[a-zA-z0-9]{1,12}$".toRegex()
+    private val passwordRegex = "[a-zA-z0-9]{10,20}$".toRegex()
 
     fun validationNickname(nickname: String): String =
         when {
             nickname.isEmpty() -> ""
             nickname.isBlank() -> ErrorMessage.EMPTY_MESSAGE
-            nickname.length < 2 -> ErrorMessage.TOO_SHORT
+            nickname.length < 2 -> ErrorMessage.NICK_TOO_SHORT
             nickname.length > 10 -> ErrorMessage.NICK_TOO_LONG
             !nickNameRegex.matches(nickname) -> ErrorMessage.NICK_NOT_MATCH
             else -> ""
@@ -18,8 +18,8 @@ object Validator {
         when {
             password.isEmpty() -> ""
             password.isBlank() -> ErrorMessage.EMPTY_MESSAGE
-            password.length < 2 -> ErrorMessage.TOO_SHORT
-            password.length > 15 -> ErrorMessage.PW_TOO_LONG
+            password.length < 10 -> ErrorMessage.PW_TOO_SHORT
+            password.length > 20 -> ErrorMessage.PW_TOO_LONG
             !passwordRegex.matches(password) -> ErrorMessage.PW_NOT_MATCH
             else -> ""
         }
