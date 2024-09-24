bbc29b34477ad043d6c3458bc983c53f7036b9db
jaehan4707 <jahan4707@naver.com>
2024-09-24 14:59:15 +0900

    feat(#S11P21D109-246): AuthRepository signUpApi 추가

--- android/data/src/main/java/com/semonemo/data/repository/AuthRepositoryImpl.kt
+++ android/data/src/main/java/com/semonemo/data/repository/AuthRepositoryImpl.kt
@@ -1,12 +1,18 @@
 package com.semonemo.data.repository
 
-import android.util.Log
+import com.google.gson.Gson
 import com.semonemo.data.network.api.AuthApi
 import com.semonemo.data.network.response.emitApiResponse
 import com.semonemo.domain.model.ApiResponse
 import com.semonemo.domain.repository.AuthRepository
+import com.semonemo.domain.request.SignUpRequest
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.File
 import javax.inject.Inject
 
 class AuthRepositoryImpl
@@ -17,19 +23,38 @@ class AuthRepositoryImpl
         override suspend fun exists(address: String): Flow<ApiResponse<Boolean>> =
             flow {
                 val response =
-                    emitApiResponse {
-                        api.exists(address)
-                    }
+                    emitApiResponse(
+                        apiResponse = { api.exists(address) },
+                        default = false,
+                    )
                 emit(response)
             }
 
         override suspend fun validateNickname(nickname: String): Flow<ApiResponse<Boolean>> =
             flow {
                 val response =
-                    emitApiResponse {
+                    emitApiResponse(apiResponse = {
                         api.validateNickname(nickname)
-                    }
-                Log.d("jaehan", "validate $nickname -> $response")
+                    }, default = false)
+                emit(response)
+            }
+
+        override suspend fun signUp(
+            data: SignUpRequest,
+            profileImage: File,
+        ): Flow<ApiResponse<Unit>> =
+            flow {
+                val requestFile = profileImage.asRequestBody("image/*".toMediaTypeOrNull())
+                val image = MultipartBody.Part.createFormData("image", profileImage.name, requestFile)
+                val requestBody =
+                    Gson().toJson(data).toRequestBody("application/json".toMediaTypeOrNull())
+                val response =
+                    emitApiResponse(apiResponse = {
+                        api.signUp(
+                            image = image,
+                            data = requestBody,
+                        )
+                    }, default = Unit)
                 emit(response)
             }
     }
--- android/domain/src/main/java/com/semonemo/domain/repository/AuthRepository.kt
+++ android/domain/src/main/java/com/semonemo/domain/repository/AuthRepository.kt
@@ -1,10 +1,17 @@
 package com.semonemo.domain.repository
 
 import com.semonemo.domain.model.ApiResponse
+import com.semonemo.domain.request.SignUpRequest
 import kotlinx.coroutines.flow.Flow
+import java.io.File
 
 interface AuthRepository {
     suspend fun exists(address: String): Flow<ApiResponse<Boolean>>
 
     suspend fun validateNickname(nickname: String): Flow<ApiResponse<Boolean>>
+
+    suspend fun signUp(
+        data: SignUpRequest,
+        profileImage: File,
+    ): Flow<ApiResponse<Unit>>
 }
