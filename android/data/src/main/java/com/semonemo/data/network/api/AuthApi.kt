04844d4f7b1b1f666ed45f2bbf61398bb7d676e7
jaehan4707 <jahan4707@naver.com>
2024-09-24 14:55:30 +0900

    feat(#S11P21D109-246): 회원가입 API 추가

--- android/data/src/main/java/com/semonemo/data/network/api/AuthApi.kt
+++ android/data/src/main/java/com/semonemo/data/network/api/AuthApi.kt
@@ -1,7 +1,12 @@
 package com.semonemo.data.network.api
 
 import com.semonemo.data.network.response.BaseResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.http.GET
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
 import retrofit2.http.Query
 
 interface AuthApi {
@@ -14,4 +19,11 @@ interface AuthApi {
     suspend fun validateNickname(
         @Query("nickname") nickname: String,
     ): BaseResponse<Boolean>
+
+    @POST("api/auth/register")
+    @Multipart
+    suspend fun signUp(
+        @Part("data") data: RequestBody,
+        @Part image: MultipartBody.Part?,
+    ): BaseResponse<Unit>
 }
