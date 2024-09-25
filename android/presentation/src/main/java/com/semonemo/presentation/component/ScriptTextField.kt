de815b2c69c52a77d71e94896f4e17c367c2cb8b
NAKYUNG LEE <lnk12888@naver.com>
2024-09-25 14:29:57 +0900

    ui(#S11P21D109-197): ScriptTextField 설명 주석 추가 및 키보드 설정

--- android/presentation/src/main/java/com/semonemo/presentation/component/ScriptTextField.kt
+++ android/presentation/src/main/java/com/semonemo/presentation/component/ScriptTextField.kt
@@ -4,13 +4,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.semonemo.presentation.theme.Gray02
@@ -18,6 +22,16 @@ import com.semonemo.presentation.theme.GunMetal
 import com.semonemo.presentation.theme.Typography
 import com.semonemo.presentation.theme.WhiteGray
 
+/**
+ * TODO
+ *
+ * @param modifier
+ * @param placeholder : 플레이스홀더 텍스트
+ * @param height : 텍스트필드의 최소 높이
+ * @param value : 입력된 텍스트
+ * @param onValueChange : 입력 후 변화
+ * @param focusManager : 포커스매니저
+ */
 @Composable
 fun ScriptTextField(
     modifier: Modifier = Modifier,
@@ -25,6 +39,7 @@ fun ScriptTextField(
     height: Int,
     value: String,
     onValueChange: (String) -> Unit,
+    focusManager: FocusManager,
 ) {
     Box(modifier = modifier.fillMaxWidth()) {
         OutlinedTextField(
@@ -38,15 +53,23 @@ fun ScriptTextField(
                     onValueChange(it)
                 }
             },
-            textStyle = Typography.labelSmall.copy(fontSize = 13.sp),
+            textStyle = Typography.labelSmall.copy(fontSize = 15.sp),
             placeholder = {
                 Text(
                     modifier = Modifier.align(Alignment.CenterStart),
                     text = placeholder,
-                    style = Typography.labelSmall.copy(fontSize = 13.sp),
+                    style = Typography.labelSmall.copy(fontSize = 15.sp),
                     color = Gray02,
                 )
             },
+            keyboardOptions =
+                KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done,
+                ),
+            keyboardActions =
+                KeyboardActions(onDone = {
+                    focusManager.clearFocus()
+                }),
             shape = RoundedCornerShape(10.dp),
             colors =
                 OutlinedTextFieldDefaults.colors(
