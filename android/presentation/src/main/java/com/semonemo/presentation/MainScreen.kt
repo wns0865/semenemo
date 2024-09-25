78203495698e167b3f610cf7b8b56df2ff64baaf
jaehan4707 <jahan4707@naver.com>
2024-09-25 15:59:48 +0900

    feat(#S11P21D109-248): 회원가입 성공 시 로그인화면으로 이동

--- android/presentation/src/main/java/com/semonemo/presentation/MainScreen.kt
+++ android/presentation/src/main/java/com/semonemo/presentation/MainScreen.kt
@@ -2,7 +2,6 @@ package com.semonemo.presentation
 
 import BottomNavigationBar
 import android.net.Uri
-import android.util.Log
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -151,13 +150,6 @@ fun MainNavHost(
                 modifier = modifier,
                 popUpBackStack = navController::popBackStack,
                 onShowErrorSnackBar = onShowErrorSnackBar,
-                navigateToMain = {
-                    navController.navigate(ScreenDestinations.Moment.route) {
-                        popUpTo(navController.graph.startDestinationId) {
-                            inclusive = true
-                        }
-                    }
-                },
             )
         }
 
