26563e31391a75503cccfa6d713243dd8217c1ce
jaehan4707 <jahan4707@naver.com>
2024-09-25 16:00:03 +0900

    feat(#S11P21D109-248): RepositoryModule TokenRepository 제거

--- android/data/src/main/java/com/semonemo/data/di/RepositoryModule.kt
+++ android/data/src/main/java/com/semonemo/data/di/RepositoryModule.kt
@@ -2,28 +2,19 @@ package com.semonemo.data.di
 
 import com.semonemo.data.repository.AuthRepositoryImpl
 import com.semonemo.data.repository.NFTRepositoryImpl
-import com.semonemo.data.repository.TokenRepositoryImpl
 import com.semonemo.domain.repository.AuthRepository
 import com.semonemo.domain.repository.NFTRepository
-import com.semonemo.domain.repository.TokenRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 interface RepositoryModule {
-    @Singleton
     @Binds
     fun provideNFTRepository(nftRepositoryImpl: NFTRepositoryImpl): NFTRepository
 
-    @Singleton
     @Binds
     fun provideAuthRepository(authRepositoryImpl: AuthRepositoryImpl): AuthRepository
-
-    @Singleton
-    @Binds
-    fun provideTokenRepository(tokenRepositoryImpl: TokenRepositoryImpl): TokenRepository
 }
