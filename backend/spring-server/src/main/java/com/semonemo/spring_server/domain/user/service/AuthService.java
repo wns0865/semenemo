9437fa2754290e3814ab1ea061575e1d62728af5
이지언 <dlwldjs7544@naver.com>
2024-09-27 12:32:41 +0900

    refactor(#S11P21D109-139) : 예외 처리 개선

--- backend/spring-server/src/main/java/com/semonemo/spring_server/domain/user/service/AuthService.java
+++ backend/spring-server/src/main/java/com/semonemo/spring_server/domain/user/service/AuthService.java
@@ -5,7 +5,7 @@ import com.semonemo.spring_server.domain.user.dto.response.UserLoginResponseDTO;
 
 public interface AuthService {
 
-	String registerUser(UserRegisterRequestDTO requestDTO);
+	void registerUser(UserRegisterRequestDTO requestDTO);
 
 	boolean existsByAddress(String address);
 
