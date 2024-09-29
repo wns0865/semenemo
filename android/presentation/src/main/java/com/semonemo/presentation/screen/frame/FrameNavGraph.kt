f6efe8a699794c7d375b789095480e556341c8ef
jaehan4707 <jahan4707@naver.com>
2024-09-30 00:21:19 +0900

    feat(#S11P21D109-200): FrameNavGraph 추가

---
+++ android/presentation/src/main/java/com/semonemo/presentation/screen/frame/FrameNavGraph.kt
@@ -0,0 +1,46 @@
+package com.semonemo.presentation.screen.frame
+
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.semonemo.presentation.navigation.ScreenDestinations
+
+fun NavGraphBuilder.FrameNavGraph(
+    modifier: Modifier,
+    navController: NavController,
+    graphRoute: String,
+    onErrorSnackBar: (String) -> Unit,
+) {
+    navigation(startDestination = ScreenDestinations.Frame.route, route = graphRoute) {
+        composable(
+            route = ScreenDestinations.Frame.route,
+        ) {
+            val viewModel =
+                hiltViewModel<FrameViewModel>(
+                    navController.getBackStackEntry(graphRoute),
+                )
+            FrameRoute(
+                modifier = modifier,
+                viewModel = viewModel,
+                navigateToFrameDone = { navController.navigate(ScreenDestinations.FrameDone.route) },
+                onErrorSnackBar = onErrorSnackBar,
+            )
+        }
+
+        composable(
+            route = ScreenDestinations.FrameDone.route,
+        ) {
+            val viewModel =
+                hiltViewModel<FrameViewModel>(
+                    navController.getBackStackEntry(graphRoute),
+                )
+            FrameDoneRoute(
+                modifier = modifier,
+                viewModel = viewModel,
+            )
+        }
+    }
+}
