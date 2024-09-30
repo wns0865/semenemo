4f6e825e95c4e6d15db2c759ed7beebd2c6f6bb8
jaehan4707 <jahan4707@naver.com>
2024-09-30 17:17:26 +0900

    feat(#S11P21D109-300): FrameUiState tags 추가

--- android/presentation/src/main/java/com/semonemo/presentation/screen/frame/FrameUiState.kt
+++ android/presentation/src/main/java/com/semonemo/presentation/screen/frame/FrameUiState.kt
@@ -1,10 +1,13 @@
 package com.semonemo.presentation.screen.frame
 
 import android.graphics.Bitmap
+import androidx.compose.runtime.Stable
 
+@Stable
 data class FrameUiState(
     val bitmap: Bitmap? = null,
     val isLoading: Boolean = false,
     val title: String = "",
     val content: String = "",
+    val tags: List<String> = listOf(),
 )
