09b98ebca9486949995d7c122c07271471099538
LeeJieon <dlwldjs7544@naver.com>
2024-09-30 20:56:11 +0900

    refactor(#S11P21D109-135) : Swagger Authorization 헤더 설정 추가

--- backend/spring-server/src/main/java/com/semonemo/spring_server/config/SwaggerConfig.java
+++ backend/spring-server/src/main/java/com/semonemo/spring_server/config/SwaggerConfig.java
@@ -1,10 +1,13 @@
 package com.semonemo.spring_server.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.media.Content;
 import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -19,8 +22,21 @@ public class SwaggerConfig {
 			.version("v1.0")
 			.title("세모내모 Spring Server API")
 			.description("세모내모 서비스의 Spring Server API");
+
+		SecurityScheme apiKey = new SecurityScheme()
+			.type(SecurityScheme.Type.HTTP)
+			.in(SecurityScheme.In.HEADER)
+			.name("Authorization")
+			.scheme("bearer")
+			.bearerFormat("JWT");
+
+		SecurityRequirement securityRequirement = new SecurityRequirement()
+			.addList("Bearer Token");
+
 		return new OpenAPI()
-			.info(info);
+			.info(info)
+			.components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+			.addSecurityItem(securityRequirement);
 	}
 
 	@Bean
