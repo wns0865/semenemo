da88bdf98b2055b4b4923d47970ec4fe4856bc49
jaehan4707 <jahan4707@naver.com>
2024-09-30 16:41:29 +0900

    feat(#S11P21D109-299): NftApi uploadImage, uploadFrame 추가

--- android/data/src/main/java/com/semonemo/data/network/api/NFTApi.kt
+++ android/data/src/main/java/com/semonemo/data/network/api/NFTApi.kt
@@ -1,13 +1,30 @@
 package com.semonemo.data.network.api
 
+import com.semonemo.domain.model.IpfsResponse
 import com.semonemo.domain.model.Transaction
 import com.semonemo.domain.request.TransferRequest
+import okhttp3.MultipartBody
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface NFTApi {
     @POST("bcapi/coin/transfer")
     suspend fun transfer(
         @Body request: TransferRequest,
     ): Transaction
+
+    @Multipart
+    @POST("api/v0/add")
+    suspend fun uploadImage(
+        @Part file: MultipartBody.Part,
+    ): Response<IpfsResponse>
+
+    @Multipart
+    @POST("api/v0/add")
+    suspend fun uploadFrame(
+        @Part file: MultipartBody.Part,
+    ): Response<IpfsResponse>
 }
