7c7de7a246407ee29aac789558a3b0c98caae8fa
nakyung128 <lnk12888@naver.com>
2024-10-01 23:03:26 +0900

    feat(#S11P21D109-267): UserListNavType 클래스 생성하여 인자 팔로우/잉 목록 전달

--- android/presentation/src/main/java/com/semonemo/presentation/navigation/ScreenDestinations.kt
+++ android/presentation/src/main/java/com/semonemo/presentation/navigation/ScreenDestinations.kt
@@ -1,7 +1,12 @@
 package com.semonemo.presentation.navigation
 
+import android.net.Uri
+import android.os.Bundle
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.semonemo.domain.model.User
 
 /*
  추가 하는 방법
@@ -51,6 +56,27 @@ sealed class ScreenDestinations(
             )
 
         fun createRoute(userId: Long) = "mypage/$userId"
+    }
+
+    data object Setting : ScreenDestinations(route = "setting")
+
+    data object FollowList : ScreenDestinations(route = "followList") {
+        override val route: String
+            get() = "followList/{nickname}/{followerList}/{followingList}"
+        val arguments =
+            listOf(
+                navArgument(name = "nickname") { type = NavType.StringType },
+                navArgument(name = "followerList") { type = UserListNavType() },
+                navArgument(name = "followingList") { type = UserListNavType() },
+            )
+
+        fun createRoute(
+            nickname: String,
+            followerList: List<User>,
+            followingList: List<User>,
+        ) = "followList/$nickname/${
+            Uri.encode(Gson().toJson(followerList))
+        }/${Uri.encode(Gson().toJson(followingList))}"
     }
 
     data object AiAsset : ScreenDestinations(route = "aiAsset")
@@ -123,4 +149,25 @@ sealed class ScreenDestinations(
     data object FrameDone : ScreenDestinations(route = "frameDone")
 
     data object Search : ScreenDestinations(route = "search")
+}
+
+// 팔로워 / 팔로잉 목록 전달 위한 NavType 정의
+class UserListNavType : NavType<List<User>>(isNullableAllowed = false) {
+    override fun get(
+        bundle: Bundle,
+        key: String,
+    ): List<User>? = bundle.getParcelableArrayList(key)
+
+    override fun parseValue(value: String): List<User> {
+        val listType = object : TypeToken<List<User>>() {}.type
+        return Gson().fromJson(value, listType)
+    }
+
+    override fun put(
+        bundle: Bundle,
+        key: String,
+        value: List<User>,
+    ) {
+        bundle.putParcelableArrayList(key, ArrayList(value))
+    }
 }
