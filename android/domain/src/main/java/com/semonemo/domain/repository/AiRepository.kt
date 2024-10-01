83ff4bfad6d60a52edb919d047a8cd1f5ee2570a
jaehan4707 <jahan4707@naver.com>
2024-10-01 16:27:23 +0900

    feat(#S11P21D109-302): removeBackground Api 연동

--- android/domain/src/main/java/com/semonemo/domain/repository/AiRepository.kt
+++ android/domain/src/main/java/com/semonemo/domain/repository/AiRepository.kt
@@ -1,9 +1,12 @@
 package com.semonemo.domain.repository
 
 import com.semonemo.domain.model.ApiResponse
+import com.semonemo.domain.request.RemoveBgRequest
 import com.semonemo.domain.request.makeAiAsset.MakeAiAssetRequest
 import kotlinx.coroutines.flow.Flow
 
 interface AiRepository {
     suspend fun makeAiAsset(request: MakeAiAssetRequest): Flow<ApiResponse<String>>
+
+    suspend fun removeBg(request: RemoveBgRequest): Flow<ApiResponse<String>>
 }
