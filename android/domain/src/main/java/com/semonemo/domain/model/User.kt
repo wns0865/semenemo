80bdc34335646367274bc0d0add52148dd665667
nakyung128 <lnk12888@naver.com>
2024-10-01 23:02:01 +0900

    feat(#S11P21D109-267): @Percelize 어노테이션 추가

--- android/domain/src/main/java/com/semonemo/domain/model/User.kt
+++ android/domain/src/main/java/com/semonemo/domain/model/User.kt
@@ -1,8 +1,12 @@
 package com.semonemo.domain.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class User(
     val userId: Long = 0L,
     val address: String = "",
     val nickname: String = "",
     val profileImage: String = "",
-)
+) : Parcelable
