943b49170308a4bdb465779b66852703d49f5c4c
jaehan4707 <jahan4707@naver.com>
2024-10-03 20:21:50 +0900

    feat(#S11P21D109-326): FrameType idx 값으로 타입 반환 추가

--- android/presentation/src/main/java/com/semonemo/presentation/screen/frame/FrameType.kt
+++ android/presentation/src/main/java/com/semonemo/presentation/screen/frame/FrameType.kt
@@ -4,7 +4,12 @@ enum class FrameType(
     val idx: Int,
     val amount: Int,
 ) {
-    OneByOne(1),
-    TwoByTwo(2),
-    OneByFour(3),
+    OneByOne(1, 1),
+    TwoByTwo(2, 4),
+    OneByFour(3, 4),
+    ;
+
+    companion object {
+        fun fromIdx(idx: Int): FrameType? = values().find { it.idx == idx }
+    }
 }
