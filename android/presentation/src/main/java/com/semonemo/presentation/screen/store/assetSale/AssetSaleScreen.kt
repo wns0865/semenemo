414a7730e105bfc65ce18ac6baafd2337b35bb2f
nakyung128 <lnk12888@naver.com>
2024-10-04 00:58:04 +0900

    feat(#S11P21D109-324): AssetSaleScreen 통신 코드 추가

--- android/presentation/src/main/java/com/semonemo/presentation/screen/store/assetSale/AssetSaleScreen.kt
+++ android/presentation/src/main/java/com/semonemo/presentation/screen/store/assetSale/AssetSaleScreen.kt
@@ -1,4 +1,4 @@
-package com.semonemo.presentation.screen.store
+package com.semonemo.presentation.screen.store.assetSale
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
@@ -7,7 +7,6 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -39,7 +38,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -55,8 +54,14 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.semonemo.domain.model.Asset
+import com.semonemo.domain.model.SellAsset
 import com.semonemo.presentation.R
 import com.semonemo.presentation.component.HashTag
+import com.semonemo.presentation.component.HashTagTextField
+import com.semonemo.presentation.component.LoadingDialog
 import com.semonemo.presentation.component.LongBlackButton
 import com.semonemo.presentation.component.LongUnableButton
 import com.semonemo.presentation.component.PriceTextField
@@ -66,34 +71,68 @@ import com.semonemo.presentation.theme.Gray03
 import com.semonemo.presentation.theme.SemonemoTheme
 import com.semonemo.presentation.theme.Typography
 import com.semonemo.presentation.util.addFocusCleaner
+import com.skydoves.landscapist.glide.GlideImage
+
+@Composable
+fun AssetSaleRoute(
+    modifier: Modifier,
+    viewModel: AssetSaleViewModel = hiltViewModel(),
+    navigateToStore: () -> Unit,
+) {
+    val assetState by viewModel.assetState.collectAsStateWithLifecycle()
+    HandleAssetSaleUi(
+        modifier = modifier,
+        assetState = assetState,
+        onSaleButtonClick = viewModel::sellAsset,
+        navigateToStore = navigateToStore,
+    )
+}
+
+@Composable
+fun HandleAssetSaleUi(
+    modifier: Modifier,
+    assetState: AssetSaleState,
+    onSaleButtonClick: (SellAsset) -> Unit,
+    navigateToStore: () -> Unit,
+) {
+    when (assetState) {
+        AssetSaleState.Init -> LoadingDialog()
+
+        is AssetSaleState.LoadSuccess -> {
+            AssetSaleScreen(
+                modifier = modifier,
+                assets = assetState.assets,
+                onSaleButtonClick = onSaleButtonClick,
+            )
+        }
+
+        AssetSaleState.SellSuccess -> {
+            navigateToStore()
+        }
+
+        else -> {}
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AssetSaleScreen() {
+fun AssetSaleScreen(
+    modifier: Modifier = Modifier,
+    assets: List<Asset> = emptyList(),
+    onSaleButtonClick: (SellAsset) -> Unit = {},
+) {
     val focusManager = LocalFocusManager.current
     val scrollState = rememberScrollState()
 
     var price by remember { mutableStateOf("") } // 판매가
     var showBottomSheet by remember { mutableStateOf(false) } // bottomSheet 보임 여부
-    var selectedIndex by remember { mutableIntStateOf(0) } // bottomSheet 선택된 탭 index
-    var selectedAsset by remember { mutableIntStateOf(-1) } // 선택된 에셋
-
-    // 더미 데이터
-    val tags = listOf("병아리", "삐약이")
-
-    val assets =
-        listOf(
-            R.drawable.img_example,
-            R.drawable.img_example2,
-            R.drawable.img_example3,
-            R.drawable.img_example,
-            R.drawable.img_example2,
-            R.drawable.img_example3,
-        )
+    var selectedAsset by remember { mutableStateOf("") } // 선택된 에셋
+    var asset by remember { mutableStateOf<Asset?>(null) }
+    val tags = remember { mutableStateListOf<String>() }
 
     Surface(
         modifier =
-            Modifier
+            modifier
                 .fillMaxSize()
                 .background(color = Color.White)
                 .verticalScroll(state = scrollState),
@@ -128,10 +167,9 @@ fun AssetSaleScreen() {
                     showBottomSheet = true
                 },
             ) {
-                if (selectedAsset != -1) {
-                    Image(
-                        painter = painterResource(id = selectedAsset),
-                        contentDescription = "img_example",
+                if (selectedAsset != "") {
+                    GlideImage(
+                        imageModel = selectedAsset,
                         contentScale = ContentScale.Crop,
                     )
                 } else {
@@ -159,7 +197,7 @@ fun AssetSaleScreen() {
             Spacer(modifier = Modifier.height(30.dp))
             // 통신 성공인 경우
             AnimatedVisibility(
-                visible = selectedAsset != -1,
+                visible = selectedAsset != "",
                 enter = fadeIn() + expandVertically(),
                 exit = fadeOut() + shrinkVertically(),
             ) {
@@ -182,6 +220,15 @@ fun AssetSaleScreen() {
                         )
                     }
                     Spacer(modifier = Modifier.height(10.dp))
+                    HashTagTextField(
+                        focusManager = focusManager,
+                        onTagAddAction = { keyword ->
+                            if (keyword.isNotBlank()) {
+                                tags.add(keyword)
+                            }
+                        },
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
                     LazyRow(
                         modifier =
                             Modifier
@@ -192,6 +239,10 @@ fun AssetSaleScreen() {
                             items(tags.size) { index ->
                                 HashTag(
                                     keyword = tags[index],
+                                    isEdit = true,
+                                    onCloseClicked = {
+                                        tags.remove(it)
+                                    },
                                 )
                             }
                         },
@@ -222,17 +273,27 @@ fun AssetSaleScreen() {
                     price = newPrice
                 },
             )
-            // 프레임 불러오기 Success면 LongBlackButton
-            // 다른 상태면 LongUnableButton
             Spacer(modifier = Modifier.height(30.dp))
-            if (selectedAsset != -1) {
+            if (selectedAsset != "") {
                 LongBlackButton(
                     modifier =
                         Modifier
                             .fillMaxWidth(),
                     text = stringResource(R.string.register_btn_title),
                     icon = null,
-                    onClick = { },
+                    onClick = {
+                        val sellAsset =
+                            asset?.let {
+                                SellAsset(
+                                    assetId = it.assetId,
+                                    price = price.toInt(),
+                                    tags = tags,
+                                )
+                            }
+                        sellAsset?.let {
+                            onSaleButtonClick(sellAsset)
+                        }
+                    },
                 )
             } else {
                 LongUnableButton(
@@ -267,10 +328,7 @@ fun AssetSaleScreen() {
                         state = rememberLazyGridState(),
                     ) {
                         items(assets.size) { index ->
-                            Image(
-                                painter = painterResource(id = assets[index]),
-                                contentDescription = null,
-                                contentScale = ContentScale.Crop,
+                            GlideImage(
                                 modifier =
                                     Modifier
                                         .fillMaxWidth()
@@ -282,9 +340,12 @@ fun AssetSaleScreen() {
                                             shape = RoundedCornerShape(10.dp),
                                             color = Gray03,
                                         ).clickable {
-                                            selectedAsset = assets[index]
+                                            selectedAsset = assets[index].imageUrl
+                                            asset = assets[index]
                                             showBottomSheet = false
                                         },
+                                imageModel = assets[index].imageUrl,
+                                contentScale = ContentScale.Crop,
                             )
                         }
                     }
