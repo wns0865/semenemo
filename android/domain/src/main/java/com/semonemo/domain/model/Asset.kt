1610852de15e10a4a04368265d1c7fe2e6501143
nakyung128 <lnk12888@naver.com>
2024-10-04 00:56:41 +0900

    feat(#S11P21D109-324): 에셋 관련 데이터 클래스 추가

--- android/domain/src/main/java/com/semonemo/domain/model/Asset.kt
+++ android/domain/src/main/java/com/semonemo/domain/model/Asset.kt
@@ -6,3 +6,29 @@ data class Asset(
     val creator: Long = 0L,
     val isLiked: Boolean = false,
 )
+
+data class SellAsset(
+    val assetId: Long = 0,
+    val price: Int = 0,
+    val tags: List<String>,
+)
+
+data class SellAssetDetail(
+    val assetId: Long = 0,
+    val assetSellId: Long = 0,
+    val creator: Long = 0,
+    val imageUrl: String = "",
+    val createAt: String = "",
+    val hits: Long = 0,
+    val likeCount: Long = 0,
+    val nickname: String = "",
+    val price: Long = 0,
+    val isLiked: Boolean = false,
+    val tags: List<String> = emptyList(),
+)
+
+data class CreateAsset(
+    val content: List<Asset> = emptyList(),
+    val nextCursor: Long? = null,
+    val hasNext: Boolean = false,
+)
