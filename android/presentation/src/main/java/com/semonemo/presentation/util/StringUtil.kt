b7ed1eb835e030e1d730e3cdadec172e38f67a02
jaehan4707 <jahan4707@naver.com>
2024-10-04 00:32:14 +0900

    feat(#S11P21D109-327): BigInteger -> 10^18 바꾸는 변환 함수 추가

--- android/presentation/src/main/java/com/semonemo/presentation/util/StringUtil.kt
+++ android/presentation/src/main/java/com/semonemo/presentation/util/StringUtil.kt
@@ -3,7 +3,10 @@ package com.semonemo.presentation.util
 import android.net.Uri
 import androidx.core.net.toUri
 import com.semonemo.presentation.BuildConfig
+import java.math.BigInteger
 
 fun String.toUriOrDefault(): Uri = Uri.decode(this).let { Uri.parse(it) } ?: "".toUri()
 
 fun String.urlToIpfs() = BuildConfig.IPFS_READ_URL + "ipfs/" + this
+
+fun BigInteger.toPrice() = this * BigInteger.TEN.pow(18)
