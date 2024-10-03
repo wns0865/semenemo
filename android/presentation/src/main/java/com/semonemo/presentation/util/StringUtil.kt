de5a3115ef22364209fba026b4006a9c6d5dc0fa
jaehan4707 <jahan4707@naver.com>
2024-10-03 23:45:54 +0900

    feat(#S11P21D109-327): ipfs image Url 변환 함수 추가

--- android/presentation/src/main/java/com/semonemo/presentation/util/StringUtil.kt
+++ android/presentation/src/main/java/com/semonemo/presentation/util/StringUtil.kt
@@ -2,5 +2,8 @@ package com.semonemo.presentation.util
 
 import android.net.Uri
 import androidx.core.net.toUri
+import com.semonemo.presentation.BuildConfig
 
 fun String.toUriOrDefault(): Uri = Uri.decode(this).let { Uri.parse(it) } ?: "".toUri()
+
+fun String.urlToIpfs() = BuildConfig.IPFS_READ_URL + "ipfs/" + this
