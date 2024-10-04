49435e0d32ec550bfdc74ae59e02696d9d260d6f
nakyung128 <lnk12888@naver.com>
2024-10-04 22:00:37 +0900

    feat(#S11P21D109-338): Event, State 정의

---
+++ android/presentation/src/main/java/com/semonemo/presentation/screen/mypage/detail/DetailUiEvent.kt
@@ -0,0 +1,15 @@
+package com.semonemo.presentation.screen.mypage.detail
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Stable
+
+@Stable
+sealed interface DetailUiEvent {
+    @Immutable
+    data object Loading : DetailUiEvent
+
+    @Immutable
+    data class Error(
+        val errorMessage: String = "",
+    ) : DetailUiEvent
+}
