944761a9a65ce67e4646f75ebcad0a11dce54a91
최현성 <huns1013@gmail.com>
2024-10-04 16:37:18 +0900

    feat(#S11P21D109-284): 거래내역 조회 구현

--- backend/spring-server/src/main/java/com/semonemo/spring_server/domain/coin/repository/TradeLogRepositoryImpl.java
+++ backend/spring-server/src/main/java/com/semonemo/spring_server/domain/coin/repository/TradeLogRepositoryImpl.java
@@ -1,4 +1,46 @@
 package com.semonemo.spring_server.domain.coin.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.semonemo.spring_server.domain.coin.entity.TradeLog;
+import com.semonemo.spring_server.domain.coin.entity.QTradeLog;
+import com.semonemo.spring_server.domain.nft.entity.NFTMarket;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
 public class TradeLogRepositoryImpl implements TradeLogRepositoryCustom {
+    private final JPAQueryFactory queryFactory;
+
+    public TradeLogRepositoryImpl(JPAQueryFactory queryFactory) {
+        this.queryFactory = queryFactory;
+    }
+
+    QTradeLog tradeLog = QTradeLog.tradeLog;
+
+    @Override
+    public Page<TradeLog> findByUser(Long userId, Pageable pageable) {
+        List<TradeLog> content = queryFactory
+            .selectFrom(tradeLog)
+            .where(
+                tradeLog.fromUser.id.eq(userId)
+                    .or(tradeLog.toUser.id.eq(userId))
+            )
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .fetch();
+
+        long total = Optional.ofNullable(queryFactory
+            .select(tradeLog.count())
+            .from(tradeLog)
+            .where(
+                tradeLog.fromUser.id.eq(userId)
+                    .or(tradeLog.toUser.id.eq(userId))
+            )
+            .fetchOne()).orElse(0L);
+
+        return new PageImpl<>(content, pageable, total);
+    }
 }
