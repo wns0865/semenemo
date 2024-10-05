ad024b75820efb772c42ec21c484e00dc402aca9
jaehan4707 <jahan4707@naver.com>
2024-10-05 19:43:30 +0900

    feat(#S11P21D109-341): FrameDetailEvent LoadCoin, Success 추가

--- android/presentation/src/main/java/com/semonemo/presentation/screen/detail/frame/FrameDetailUiEvent.kt
+++ android/presentation/src/main/java/com/semonemo/presentation/screen/detail/frame/FrameDetailUiEvent.kt
@@ -2,6 +2,7 @@ package com.semonemo.presentation.screen.detail.frame
 
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.Stable
+import com.semonemo.domain.model.Coin
 
 @Stable
 sealed interface FrameDetailUiEvent {
@@ -9,4 +10,12 @@ sealed interface FrameDetailUiEvent {
     data class Error(
         val errorMessage: String,
     ) : FrameDetailUiEvent
+
+    @Immutable
+    data class LoadCoin(
+        val coin: Coin,
+    ) : FrameDetailUiEvent
+
+    @Immutable
+    data object Success : FrameDetailUiEvent
 }
