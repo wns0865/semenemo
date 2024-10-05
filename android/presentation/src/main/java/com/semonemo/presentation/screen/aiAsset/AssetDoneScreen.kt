3a24e6875aeee31241a5f2037b4b5c97521a9b81
nakyung128 <lnk12888@naver.com>
2024-10-05 21:46:57 +0900

    ui(#S11P21D109-337): 에셋 제작 완료 화면 이미지 크기 수정

--- android/presentation/src/main/java/com/semonemo/presentation/screen/aiAsset/AssetDoneScreen.kt
+++ android/presentation/src/main/java/com/semonemo/presentation/screen/aiAsset/AssetDoneScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,8 +17,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -118,9 +117,9 @@ fun AssetDoneScreen(
         modifier =
             modifier
                 .fillMaxSize()
+                .background(color = Color.White)
                 .statusBarsPadding()
                 .navigationBarsPadding()
-                .background(color = Color.White)
                 .addFocusCleaner(
                     focusManager = focusManager,
                 ),
@@ -162,16 +161,16 @@ fun AssetDoneScreen(
                 color = Gray02,
                 style = Typography.labelLarge.copy(fontSize = 16.sp),
             )
-            Spacer(modifier = Modifier.height(20.dp))
+            Spacer(modifier = Modifier.height(10.dp))
             GlideImage(
                 imageModel = assetUrl,
                 modifier =
                     Modifier
-                        .fillMaxWidth(0.6f)
-                        .fillMaxHeight(0.5f),
-                contentScale = ContentScale.Crop,
+                        .fillMaxWidth()
+                        .aspectRatio(1f),
+                contentScale = ContentScale.Fit,
             )
-            Spacer(modifier = Modifier.fillMaxHeight(0.45f))
+            Spacer(modifier = Modifier.fillMaxHeight(0.1f))
             Row(modifier = Modifier.fillMaxWidth(0.88f)) {
                 LongBlackButton(
                     modifier = Modifier.weight(1f),
@@ -194,7 +193,6 @@ fun AssetDoneScreen(
                     },
                 )
             }
-            Spacer(modifier = Modifier.height(13.dp))
             Spacer(modifier = Modifier.height(13.dp))
             LongWhiteButton(
                 icon = null,
