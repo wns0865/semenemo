85643d8cbaf7f4d00e86004e990ae3ec72036320
전형선 <hsun5163@gmail.com>
2024-10-06 19:17:35 +0900

    feat(#S11P21D109-270): 데이터 필드 추가

--- android/domain/src/main/java/com/semonemo/domain/model/AuctionBidLog.kt
+++ android/domain/src/main/java/com/semonemo/domain/model/AuctionBidLog.kt
@@ -4,6 +4,7 @@ import java.time.LocalDateTime
 
 data class AuctionBidLog(
     val userId: Long = 0L,
+    val anonym: Int = 0,
     val bidAmount: Long = 0,
     val bidTime: LocalDateTime = LocalDateTime.now(),
     val endTime: LocalDateTime = LocalDateTime.now(),
