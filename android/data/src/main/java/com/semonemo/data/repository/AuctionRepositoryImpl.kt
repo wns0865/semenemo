f7b37eff6b571dadce73dea17cafd9aa286731cb
전형선 <hsun5163@gmail.com>
2024-10-09 17:58:58 +0900

    fix(#S11P21D109-400): 경매 조회 수정 및 시간 파싱 해결

--- android/data/src/main/java/com/semonemo/data/repository/AuctionRepositoryImpl.kt
+++ android/data/src/main/java/com/semonemo/data/repository/AuctionRepositoryImpl.kt
@@ -1,15 +1,20 @@
 package com.semonemo.data.repository
 
+import android.util.Log
 import com.semonemo.data.network.api.AuctionApi
 import com.semonemo.data.network.response.emitApiResponse
 import com.semonemo.domain.model.ApiResponse
 import com.semonemo.domain.model.Auction
+import com.semonemo.domain.model.AuctionBidLog
+import com.semonemo.domain.model.AuctionJoin
 import com.semonemo.domain.model.AuctionJoinResponse
 import com.semonemo.domain.model.AuctionRegisterRequest
 import com.semonemo.domain.model.AuctionRegisterResponse
 import com.semonemo.domain.repository.AuctionRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
 class AuctionRepositoryImpl
@@ -66,16 +71,37 @@ class AuctionRepositoryImpl
                 }
             }
 
-        override suspend fun joinAuction(auctionId: Long): Flow<ApiResponse<AuctionJoinResponse>> =
+        override suspend fun joinAuction(auctionId: Long): Flow<ApiResponse<AuctionJoin>> =
             flow {
                 val response =
                     emitApiResponse(
                         apiResponse = { api.joinAuction(auctionId) },
                         default = AuctionJoinResponse(),
                     )
+                Log.d("Auction", "response : $response")
+                val dateTimFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
                 when (response) {
                     is ApiResponse.Error -> emit(response)
-                    is ApiResponse.Success -> emit(ApiResponse.Success(data = response.data))
+                    is ApiResponse.Success ->
+                        emit(
+                            ApiResponse.Success(
+                                data =
+                                    AuctionJoin(
+                                        response.data.anonym,
+                                        response.data.participants,
+                                        bidLogs =
+                                            response.data.bidLogs.map {
+                                                AuctionBidLog(
+                                                    userId = it.userId,
+                                                    anonym = it.anonym,
+                                                    bidAmount = it.bidAmount,
+                                                    bidTime = LocalDateTime.parse(it.bidTime, dateTimFormat),
+                                                    endTime = LocalDateTime.parse(it.endTime, dateTimFormat),
+                                                )
+                                            },
+                                    ),
+                            ),
+                        )
                 }
             }
 
